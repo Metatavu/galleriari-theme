f929a3b3c5f5eb63ccdddf5a2ac5a60fdddedd6f
unknown <eskilmar@hotmail.com>
2020-06-08 17:48:32 +0300

    Added elements

--- global-templates/front-page.php
+++ global-templates/front-page.php
@@ -17,6 +17,7 @@ $heroTitle = get_theme_mod( 'galleriari_hero_title' );
 $heroText = get_theme_mod( 'galleriari_hero_text_setting' );
 $heroBtn1Text = get_theme_mod( 'galleriari_hero_button1_text_setting' );
 $heroBtn1Url = get_theme_mod( 'galleriari_hero_button1_url_setting' );
+$secondaryTitle = get_theme_mod( 'secondary_title_setting' );
 ?>
 
 <div class="wrapper" id="wrapper-hero">
@@ -41,4 +42,163 @@ $heroBtn1Url = get_theme_mod( 'galleriari_hero_button1_url_setting' );
             </div>
         </div>
     </div>
-</div><!-- wrapper end -->
+</div>
+
+<!-- HERO wrapper end -->
+
+<div class="wrapper" id="page">
+	<div class="container" id="content" tabindex="-1">
+		<!-- "" => text-center"" -->
+		<div class="row text-center">
+			<!-- Do the left sidebar check -->
+            <div class="col-md content-area" id="primary">
+			    <main class="site-main" id="main">
+                    <article class="post-14 page type-page status-publish hentry" id="post-14">
+                        <header class="entry-header">
+
+                        </header><!-- .entry-header -->
+                        <div class="entry-content">
+
+                            <h2 class="has-text-align-center">
+                                <?php echo esc_attr( $secondaryTitle ); ?>
+                            </h2>
+
+                            <ul class="wp-block-latest-posts wp-block-latest-posts__list is-grid columns-3">
+
+                            <?php
+                                $the_query = new WP_Query( array( 
+                                    'orderby' => 'date',
+                                    'category_name' => 'ajankohtaista', //name of category by slug
+                                    'posts_per_page' => '3'
+                                )); // how many posts to show
+
+                                // Put into the loop
+                                while ( $the_query->have_posts() ) :
+
+                                    $the_query->the_post();
+
+                                    echo '<li><div class="wp-block-latest-posts__featured-image"><img width="150" height="150" src="'. get_the_post_thumbnail_url() .'" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" style=""></div>';
+                                    echo '<a href="' . get_permalink($post->ID) . '">' . get_the_title() . '</a></li>';
+
+                                endwhile;
+
+                                // Restore original Post Data if needed
+                                wp_reset_postdata();
+                            ?>
+                            </ul>
+                            <p></p>
+
+	                    </div><!-- .entry-content -->
+	                <footer class="entry-footer">
+	                </footer><!-- .entry-footer -->
+
+                    </article><!-- #post-## -->
+
+			    </main><!-- #main -->
+			<!-- Do the right sidebar check -->	
+
+            </div><!-- #closing the primary container from /global-templates/left-sidebar-check.php -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div>
+
+<!-- AJANKOHTAISTA -->
+
+
+<div class="wrapper" id="page-wrapper">
+	<div class="container" id="content" tabindex="-1">
+		<!-- "" => text-center"" -->
+		<div class="row text-center">
+			<!-- Do the left sidebar check -->
+            <div class="col-md content-area" id="primary">
+			    <main class="site-main" id="main">
+                    <article class="post-14 page type-page status-publish hentry" id="post-14">
+                        <header class="entry-header">
+
+                        </header><!-- .entry-header -->
+                        <div class="entry-content">
+
+                            <h2 class="has-text-align-center">
+                                <?php echo esc_attr( $secondaryTitle ); ?>
+                            </h2>
+
+                            <ul class="wp-block-latest-posts wp-block-latest-posts__list is-grid columns-3">
+
+                            <?php
+                                $the_query = new WP_Query( array( 
+                                    'orderby' => 'date',
+                                    'category_name' => 'ajankohtaista', //name of category by slug
+                                    'posts_per_page' => '3'
+                                )); // how many posts to show
+
+                                // Put into the loop
+                                while ( $the_query->have_posts() ) :
+
+                                    $the_query->the_post();
+
+                                    echo '<li><div class="wp-block-latest-posts__featured-image"><img width="150" height="150" src="'. get_the_post_thumbnail_url() .'" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" style=""></div>';
+                                    echo '<a href="' . get_permalink($post->ID) . '">' . get_the_title() . '</a></li>';
+
+                                endwhile;
+
+                                // Restore original Post Data if needed
+                                wp_reset_postdata();
+                            ?>
+                            </ul>
+                            <p></p>
+
+	                    </div><!-- .entry-content -->
+	                <footer class="entry-footer">
+	                </footer><!-- .entry-footer -->
+
+                    </article><!-- #post-## -->
+
+			    </main><!-- #main -->
+			<!-- Do the right sidebar check -->	
+
+            </div><!-- #closing the primary container from /global-templates/left-sidebar-check.php -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div>
+
+<div class="wrapper" id="wrapper-hero">
+
+    <div class="hero-image-container" style="z-index: 0;">
+    <div class="hero-video">
+    </div>
+
+
+        <?php
+            $the_query = new WP_Query( array( 
+                'orderby' => 'date',
+                'category_name' => 'nayttely', //name of category by slug
+                'posts_per_page' => '1'
+            )); // how many posts to show
+
+            // Put into the loop
+            while ( $the_query->have_posts() ) :
+
+                $the_query->the_post();
+
+                echo '<img class="video" src="'. get_the_post_thumbnail_url() .'" alt="" style="">';
+                echo '<div class="hero-banner-content-container">';
+                echo '<h1 class="hero-banner-title">'. get_the_title() .' </h1>';
+                echo '<h2 class="hero-banner-subtitle">'. nl2br(get_excerpt('excerpt_more')) .' </h2>';
+                echo '<div class="hero-banner-button"><a href="'.get_permalink($post->ID).'"  class="btn btn-light hero-banner-button btn-light" >'. $heroBtn1Text .'</a>';
+
+            endwhile;
+
+            // Restore original Post Data if needed
+            wp_reset_postdata();
+            ?>
+
+            </div>
+        </div>
+    </div>
+</div>
