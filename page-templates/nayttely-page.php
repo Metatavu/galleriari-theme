9826813280abce1ca0f5df83ab8c70b1a2283f99
Eetu Lepisto <aeele001@edu.xamk.fi>
2020-06-09 12:12:33 +0300

    Moved custom div to loop-templates

--- page-templates/nayttely-page.php
+++ page-templates/nayttely-page.php
@@ -32,7 +32,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<?php
 				while ( have_posts() ) {
 					the_post();
-					get_template_part( 'loop-templates/content', 'page' );
+					get_template_part( 'loop-templates/content', 'nayttely' );
 
 
 					// If comments are open or we have at least one comment, load up the comment template.
@@ -41,35 +41,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 					}
 				}
 				?>
-
-				<!--Custom div for displaying posts in nayttely-page-->
-				<div class="">
-					<?php
-						$the_query = new WP_Query( array( 
-							'orderby' => 'date',
-							'category_name' => 'nayttely', //name of category by slug
-							'posts_per_page' => '2'
-						)); // how many posts to show
-						// Put into the loop
-						while ( $the_query->have_posts() ) :
-							$the_query->the_post();
-							echo '<div class = "entry-content">';
-							echo '<div class = "wp-block-columns">';
-								echo '<div class="wp-block-column">';
-								echo '<figure class="wp-block-image size-large"><img class="wp-image-167" sizes="(max-width: 606px) 100vw, 606px" src="'. get_the_post_thumbnail_url() .'"</figure>';
-								echo '</div>';
-								echo '<div class="wp-block-column">';
-								echo '<h3>' . get_the_title() . '</h3>';
-								echo '<p>'. get_the_content() .'</p>';
-							echo '</div></div>';
-
-							
-
-						endwhile;
-						// Restore original Post Data if needed
-						wp_reset_postdata();
-					?>
-				</div>
 
 			</main><!-- #main -->
 
